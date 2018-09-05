9e332c5d7bcd19025d5f48b6eb5e9ccc902966fd
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-09-05 14:53:07 -0600

    finished all challenges

--- Arrays/src/arrays.java
+++ Arrays/src/arrays.java
@@ -1,11 +1,43 @@
+import java.lang.reflect.Array;
 
 public class arrays
 	{
-
+		static int[] numbers = {2, 4, 8, 16, 32};
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				doChallengeOne();
+				doChallengeTwo();
+				doChallengeThree();
+				doChallengeFour();
 			}
-
-	}
+		public static void doChallengeOne()
+			{
+				String [] soccerTeams = new String[5];
+				soccerTeams[0]= "Barcelona";
+				soccerTeams[1]= "Real Madrid";
+				soccerTeams[2]= "Atheltico Madrid";
+				soccerTeams[3]= "Valencia";
+				soccerTeams[4]= "Espanol";
+				System.out.println(soccerTeams[2]);	
+			}
+		public static void doChallengeTwo()
+			{
+				System.out.println(numbers[numbers.length -1]);
+			}
+		public static void doChallengeThree()
+			{
+				for (int i = 0; i < numbers.length; i++)
+					{
+						System.out.println(numbers[i]);
+					}
+			}
+		public static void doChallengeFour()
+			{
+				double average = 0;
+				for (int i = 0; i < numbers.length; i++)
+					{
+						average = average + numbers[i];
+					}
+				System.out.println(average/(numbers.length-1));
+			}
+	}	
